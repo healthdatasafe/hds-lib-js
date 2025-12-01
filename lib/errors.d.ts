54afeeee99fb2fe799c57c8bc9a298197da1bac1
Perki <perki@pryv.com>
2025-12-01 18:29:49 +0100

    Checking out to lib

---
+++ lib/errors.d.ts
@@ -0,0 +1,6 @@
+export declare class HDSLibError extends Error {
+    innerObject: any;
+    constructor(message: string, innerObject?: any);
+    toString(): string;
+}
+//# sourceMappingURL=errors.d.ts.map
---
+++ lib/toolkit/index.d.ts
@@ -0,0 +1,3 @@
+import { StreamsAutoCreate } from './StreamsAutoCreate';
+export { StreamsAutoCreate };
+//# sourceMappingURL=index.d.ts.map
