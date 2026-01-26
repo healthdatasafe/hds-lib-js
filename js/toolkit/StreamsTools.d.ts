8ad080a1a9b981d4a056dc6cfeec800286738c13
Perki <perki@pryv.com>
2026-01-26 18:09:11 +0100

    Adding getchildren streamIds

--- js/toolkit/StreamsTools.d.ts
+++ js/toolkit/StreamsTools.d.ts
@@ -1,5 +1,7 @@
+import type { pryv as Pryv } from '../patchedPryv';
 /**
  * Iterate all streams and children
  */
-export declare function allStreamsAndChildren(streamStructure: any): Generator<any, void, unknown>;
+export declare function allStreamsAndChildren(streamStructure: Pryv.Stream[]): Generator<Pryv.Stream, void, unknown>;
+export declare function getStreamIdAndChildrenIds(stream: Pryv.Stream): Array<string>;
 //# sourceMappingURL=StreamsTools.d.ts.map
